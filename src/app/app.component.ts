2c82686af42841731cd37f907f330f757ffb3be6
Tareq Hossain <xtrinsic96@gmail.com>
2025-02-25 20:59:25 +0600

    need to update version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Highlight } from 'ngx-highlightjs';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Highlight],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'docs';
+  code = `
+    export class AppBuilder{
+    }
+  `
 }
